f90c4571a267bfc4bf5fe3c0d290157c350167db
NyanisoTukani <139628634+NyanisoTukani@users.noreply.github.com>
2023-07-15 14:43:49 +0200

    Add files via upload

---
+++ NmaeSurnameApp.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package nmaesurnameapp;
+
+import sec.tut.ff.SecoJFrame;
+
+/**
+ *
+ * @author Nyaniso Xolilizwe
+ */
+public class NmaeSurnameApp {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        SecoJFrame ff;
+        
+       ff = new SecoJFrame();
+    }
+    
+}
---
+++ SecoJFrame.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package sec.tut.ff;
+
+import java.awt.Color;
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author Nyaniso Xolilizwe
+ */
+public class SecoJFrame extends JFrame {
+
+    public SecoJFrame(){
+        setSize(200, 250);
+        setTitle("Nyaniso Tukani");
+        setDefaultLookAndFeelDecorated(true);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setForeground(Color.blue);
+        setResizable(true);
+        setVisible(true);
+    }
+    
+    
+}
